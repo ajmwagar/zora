c984878bea1b2845c4fdf4da5bd364ec73e2742f
EC2 Default User <ec2-user@ip-172-31-35-214.us-west-2.compute.internal>
2018-08-13 21:09:56 +0000

    Remove conflict marker

--- index.js
+++ index.js
@@ -42,19 +42,11 @@ client.on("message", async message => {
 
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
-<<<<<<< HEAD
   if (message.author.bot) return;
 
   // Also good practice to ignore any message that does not start with our prefix, 
   // which is set in the configuration file.
   if (message.content.indexOf(config.prefix) !== 0) return;
-=======
-  if(message.author.bot) return;
-
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
->>>>>>> reddit
 
   // Here we separate our "command" name, and our "arguments" for the command. 
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
