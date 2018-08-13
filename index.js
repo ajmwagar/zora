5c2655589c5466be75ff9fc2f9f4808140e1a9ca
Nathan <nlaha@outlook.com>
2018-08-13 14:56:32 -0700

    changed weather to use axios

--- index.js
+++ index.js
@@ -57,23 +57,19 @@ client.on("message", async message => {
 
   // Admin
 
-  if (command === "help"){
-      // Help message
-      // Lists of current commands
-    let help = "\n\nHi there, I'm " + config.name + ".\n\nMy commands are:\n- `" + config.prefix + "help`: show this help menu\n- `" + config.prefix + "ban <user>`: ban a user (admins only)\n- `" + config.prefix + "kick <user>`: kick a user (admins and mods only)\n- `" + config.prefix + "purge <number of messages>`: purge a channel\n- `" + config.prefix + "ping`: Pong?\n- `" + config.prefix + "say <message>`: say a message\n- `" + config.prefix + "joke`: Tell a joke\n- `"+ config.prefix + "weather <city>`: Get the weather for a city\n\nHope I could help!\n\nKeep on fragging!"
+  if (command === "help") {
+    // Help message
+    // Lists of current commands
+    let help = "\n\nHi there, I'm " + config.name + ".\n\nMy commands are:\n- `" + config.prefix + "help`: show this help menu\n- `" + config.prefix + "ban <user>`: ban a user (admins only)\n- `" + config.prefix + "kick <user>`: kick a user (admins and mods only)\n- `" + config.prefix + "purge <number of messages>`: purge a channel\n- `" + config.prefix + "ping`: Pong?\n- `" + config.prefix + "say <message>`: say a message\n- `" + config.prefix + "joke`: Tell a joke\n- `" + config.prefix + "weather <city>`: Get the weather for a city\n\nHope I could help!\n\nKeep on fragging!"
 
-      // Reply to message
-     message.reply(help);
-  }
-
-  else if(command === "ping") {
+    // Reply to message
+    message.reply(help);
+  } else if (command === "ping") {
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
-
-  else if(command === "say") {
+  } else if (command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
@@ -81,13 +77,11 @@ client.on("message", async message => {
     message.delete().catch(O_o => {});
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
-  }
-
-  else if(command === "kick") {
+  } else if (command === "kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Owner", "Administrator", "Moderator"].includes(r.name)) )
+    if (!message.member.roles.some(r => ["Owner", "Administrator", "Moderator"].includes(r.name)))
       return message.reply("Sorry, you don't have permissions to use this!");
 
     // Let's first check if we have a member and if we can kick them!
@@ -109,9 +103,7 @@ client.on("message", async message => {
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
-  }
-
-  else if(command === "ban") {
+  } else if (command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
     if (!message.member.roles.some(r => ["Administrator"].includes(r.name)))
@@ -129,11 +121,9 @@ client.on("message", async message => {
     await member.ban(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-  }
-
-  else if(command === "purge") {
+  } else if (command === "purge") {
     // This command removes all messages from all users in the channel, up to 100.
-    if(!message.member.roles.some(r=>["Owner", "Administrator"].includes(r.name)) )
+    if (!message.member.roles.some(r => ["Owner", "Administrator"].includes(r.name)))
       return message.reply("Sorry, you don't have permissions to use this!");
 
     // get the delete count, as an actual number.
@@ -155,69 +145,61 @@ client.on("message", async message => {
 
   if (command === "weather") {
 
-    const city = args[0];
+    var city = args[0];
 
     if (!city) {
       return message.reply("Please provide a valid city");
     } else {
-      getJSON('http://api.apixu.com/v1/current.json?key=5d0a7d3aa80e4d5b843181446181308&q=' + city, function (error, response) {
-
-        message.channel.send("**Current Weather:**\n  Conditions: " + response.current.condition.text + "\n  Temperature: " + response.current.temp_f + "\n  Humidity: " + response.current.humidity);
+      const m = await message.channel.send("Getting Weather Data...");
+      const weather = axios.create({
+        baseURL: "http://api.apixu.com/v1/current.json?key=5d0a7d3aa80e4d5b843181446181308&q=" + city.trim(),
+        headers: {
+          Accept: "application/json"
+        }
+      });
+      weather.get("/").then(res => {
+        m.edit("**Current Weather:**\n  Conditions: " + res.data.current.condition.text + "\n  Temperature: " + res.data.current.temp_f + "\n  Humidity: " + res.data.current.humidity);
       })
-
     }
   }
 
   // Memes
 
   // TODO Add reddit implementation
-  else if (command === "addSub"){
+  else if (command === "addSub") {
     let sub = args.slice(1).join(' ');
-    if (sub){
+    if (sub) {
       return message.reply("Added " + sub);
-    }
-    else {
+    } else {
 
       return message.reply("No Subreddit provided.");
     }
 
-  }
-
-  else if (command === "removeSub"){
+  } else if (command === "removeSub") {
     let sub = args.slice(1).join(' ');
-    if (sub){
+    if (sub) {
       return message.reply("Removed " + sub);
-    }
-    else {
+    } else {
       return message.reply("No Subreddit provided.");
 
     }
-  }
+  } else if (command === "setMemeChannel") {
 
-  else if (command === "setMemeChannel"){
-
-  }
-
-  else if (command === "setMemeInterval"){
+  } else if (command === "setMemeInterval") {
 
   }
 
   // Music
 
   // TODO https://stackoverflow.com/questions/35347054/how-to-create-youtube-search-through-api
-
-  else if (command === "play"){
+  else if (command === "play") {
     // let song = args.slice(1).join(' ');
 
     // radio = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : tokens.passes });
 
-  }
+  } else if (command === "stop") {
 
-  else if (command === "stop"){
-
-  }
-
-  else if (command === "skip"){
+  } else if (command === "skip") {
 
   }
 
@@ -226,20 +208,22 @@ client.on("message", async message => {
 
   // Tell a joke using icanhazdadjoke.com (random dad jokes)
   // Use axios to create an api
-  else if (command === "joke"){
+  else if (command === "joke") {
     // Tee it up
     const m = await message.channel.send("Let me think...");
 
     // Get the joke
-    const jokeApi = axios.create( {
-        baseURL: "https://icanhazdadjoke.com",
-        headers: {
-            Accept: "application/json"
-        }
-    } );
+    const jokeApi = axios.create({
+      baseURL: "https://icanhazdadjoke.com",
+      headers: {
+        Accept: "application/json"
+      }
+    });
 
     // respond
-    jokeApi.get("/").then(res => {m.edit(res.data.joke)})
+    jokeApi.get("/").then(res => {
+      m.edit(res.data.joke)
+    })
   }
 
 
