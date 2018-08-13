b69084bebc1148840cc6f31ae09f9d80f0f29677
EC2 Default User <ec2-user@ip-172-31-35-214.us-west-2.compute.internal>
2018-08-13 21:17:59 +0000

    Update help

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on("guildCreate", guild => {
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers, prefix is set to "+"`);
+  client.user.setActivity(`${client.guilds.size} | ${config.prefix}help`);
 });
 
 
@@ -60,7 +60,7 @@ client.on("message", async message => {
   if (command === "help"){
       // Help message
       // Lists of current commands
-     let help = "\n\nHi there, I'm " + config.name + ".\n\nMy commands are:\n- `" + config.prefix + "help`: show this help menu\n- `" + config.prefix + "ban`: ban a user (admins only)\n- `" + config.prefix + "kick`: kick a user (admins and mods only)\n- `" + config.prefix + "purge`: purge a channel\n- `" + config.prefix + "ping`: Pong?\n- `" + config.prefix + "say`: say a message\n- `" + config.prefix + "joke`: Tell a joke\n\nHope I could help!\n\nKeep on fragging!"
+     let help = "\n\nHi there, I'm " + config.name + ".\n\nMy commands are:\n- `" + config.prefix + "help`: show this help menu\n- `" + config.prefix + "ban`: ban a user (admins only)\n- `" + config.prefix + "kick`: kick a user (admins and mods only)\n- `" + config.prefix + "purge`: purge a channel\n- `" + config.prefix + "ping`: Pong?\n- `" + config.prefix + "say`: say a message\n- `" + config.prefix + "joke`: Tell a joke\n- `"+ config.prefix + "weather`: Get the weather for a city\n\nHope I could help!\n\nKeep on fragging!"
 
       // Reply to message
      message.reply(help);
