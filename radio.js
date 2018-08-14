9559c235193125182cf363becd11d5c82b1a208f
Nathan <nlaha@outlook.com>
2018-08-13 22:27:44 -0700

    cleaned up radio.js

--- radio.js
+++ radio.js
@@ -5,7 +5,7 @@ const opus = require('opusscript');
 let queue = {};
 var radio;
 
-async function bot(client, message, command, args){
+async function bot(client, message, command, args) {
   const commands = {
     'play': (message) => {
       if (queue[message.guild.id] === undefined) return message.channel.send(`Add some songs to the queue first with ${config.prefix}add`);
@@ -22,24 +22,34 @@ async function bot(client, message, command, args){
           message.member.voiceChannel.leave();
         });
         message.channel.send(`Playing: **${song.title}** as requested by: **${song.requester}**`);
-        dispatcher = message.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : config.passes });
+        dispatcher = message.guild.voiceConnection.playStream(yt(song.url, {
+          audioonly: true
+        }), {
+          passes: config.passes
+        });
         let collector = message.channel.createCollector(m => m);
         collector.on('message', m => {
           if (m.content.startsWith(config.prefix + 'pause')) {
-            message.channel.send('paused').then(() => {dispatcher.pause();});
-          } else if (m.content.startsWith(config.prefix + 'resume')){
-            message.channel.send('resumed').then(() => {dispatcher.resume();});
-          } else if (m.content.startsWith(config.prefix + 'skip')){
-            message.channel.send('skipped').then(() => {dispatcher.end();});
-          } else if (m.content.startsWith(config.prefix + 'volume+')){
-            if (Math.round(dispatcher.volume*50) >= 100) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-            dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+            message.channel.send('paused').then(() => {
+              dispatcher.pause();
+            });
+          } else if (m.content.startsWith(config.prefix + 'resume')) {
+            message.channel.send('resumed').then(() => {
+              dispatcher.resume();
+            });
+          } else if (m.content.startsWith(config.prefix + 'skip')) {
+            message.channel.send('skipped').then(() => {
+              dispatcher.end();
+            });
+          } else if (m.content.startsWith(config.prefix + 'volume+')) {
+            if (Math.round(dispatcher.volume * 50) >= 100) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+            dispatcher.setVolume(Math.min((dispatcher.volume * 50 + (2 * (m.content.split('+').length - 1))) / 50, 2));
             message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-          } else if (m.content.startsWith(config.prefix + 'volume-')){
-            if (Math.round(dispatcher.volume*50) <= 0) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-            dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+          } else if (m.content.startsWith(config.prefix + 'volume-')) {
+            if (Math.round(dispatcher.volume * 50) <= 0) return message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+            dispatcher.setVolume(Math.max((dispatcher.volume * 50 - (2 * (m.content.split('-').length - 1))) / 50, 0));
             message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-          } else if (m.content.startsWith(config.prefix + 'time')){
+          } else if (m.content.startsWith(config.prefix + 'time')) {
             message.channel.send(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
           }
         });
@@ -66,16 +76,22 @@ async function bot(client, message, command, args){
       let url = message.content.split(' ')[1];
       if (url == '' || url === undefined) return message.channel.send(`You must add a YouTube video url, or id after ${config.prefix}add`);
       yt.getInfo(url, (err, info) => {
-        if(err) return message.channel.send('Invalid YouTube Link: ' + err);
+        if (err) return message.channel.send('Invalid YouTube Link: ' + err);
         if (!queue.hasOwnProperty(message.guild.id)) queue[message.guild.id] = {}, queue[message.guild.id].playing = false, queue[message.guild.id].songs = [];
-        queue[message.guild.id].songs.push({url: url, title: info.title, requester: message.author.username});
+        queue[message.guild.id].songs.push({
+          url: url,
+          title: info.title,
+          requester: message.author.username
+        });
         message.channel.send(`added **${info.title}** to the queue`);
       });
     },
     'queue': (message) => {
       if (queue[message.guild.id] === undefined) return message.channel.send(`Add some songs to the queue first with ${config.prefix}add`);
       let tosend = [];
-      queue[message.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
+      queue[message.guild.id].songs.forEach((song, i) => {
+        tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);
+      });
       message.channel.send(`__**${message.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
     },
     'reboot': (message) => {
@@ -87,4 +103,6 @@ async function bot(client, message, command, args){
 }
 
 
-module.exports = {bot};
+module.exports = {
+  bot
+};
