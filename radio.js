c3a8750484021dde0b0aaa972aa35a6338bfd71b
Nathan <nlaha@outlook.com>
2018-08-14 11:00:33 -0700

    Formatted music bot

--- radio.js
+++ radio.js
@@ -8,20 +8,57 @@ var radio;
 async function bot(client, message, command, args) {
   const commands = {
     'play': (message) => {
-      if (queue[message.guild.id] === undefined) return message.channel.send(`Add some songs to the queue first with ${config.prefix}add`);
+      if (queue[message.guild.id] === undefined) {
+        return message.channel.send({
+          embed: {
+            color: 15844367,
+            description: `Add some songs to the queue first with ${config.prefix}add`
+          }
+        });
+      }
       if (!message.guild.voiceConnection) return commands.join(message).then(() => commands.play(message));
-      if (queue[message.guild.id].playing) return message.channel.send('Already Playing');
+      if (queue[message.guild.id].playing) {
+        return message.channel.send({
+          embed: {
+            color: 15844367,
+            description: "Already Playing!"
+          }
+        });
+      }
       let dispatcher;
       queue[message.guild.id].playing = true;
 
       console.log(queue);
       (function play(song) {
         console.log(song);
-        if (song === undefined) return message.channel.send('Queue is empty').then(() => {
-          queue[message.guild.id].playing = false;
-          message.member.voiceChannel.leave();
+        if (song === undefined) {
+          return message.channel.send({
+            embed: {
+              color: 15844367,
+              description: "Queue is empty"
+            }
+          }).then(() => {
+            queue[message.guild.id].playing = false;
+            message.member.voiceChannel.leave();
+          });
+        }
+        message.channel.send({
+          embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: `▶️ Playing song requested by **${song.requester}**`,
+            url: song.url,
+            description: `Song Name: **${song.title}**`,
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: "© " + message.guild
+            }
+          }
         });
-        message.channel.send(`Playing: **${song.title}** as requested by: **${song.requester}**`);
         dispatcher = message.guild.voiceConnection.playStream(yt(song.url, {
           audioonly: true
         }), {
@@ -30,15 +67,30 @@ async function bot(client, message, command, args) {
         let collector = message.channel.createCollector(m => m);
         collector.on('message', m => {
           if (m.content.startsWith(config.prefix + 'pause')) {
-            message.channel.send('paused').then(() => {
+            message.channel.send({
+              embed: {
+                color: 3447003,
+                description: "⏸️ Music Paused"
+              }
+            }).then(() => {
               dispatcher.pause();
             });
           } else if (m.content.startsWith(config.prefix + 'resume')) {
-            message.channel.send('resumed').then(() => {
+            message.channel.send({
+              embed: {
+                color: 3447003,
+                description: "▶️ Music Resumed"
+              }
+            }).then(() => {
               dispatcher.resume();
             });
           } else if (m.content.startsWith(config.prefix + 'skip')) {
-            message.channel.send('skipped').then(() => {
+            message.channel.send({
+              embed: {
+                color: 3447003,
+                description: "⏩ Song Skipped!"
+              }
+            }).then(() => {
               dispatcher.end();
             });
           } else if (m.content.startsWith(config.prefix + 'volume+')) {
@@ -50,7 +102,12 @@ async function bot(client, message, command, args) {
             dispatcher.setVolume(Math.max((dispatcher.volume * 50 - (2 * (m.content.split('-').length - 1))) / 50, 0));
             message.channel.send(`Volume: ${Math.round(dispatcher.volume*50)}%`);
           } else if (m.content.startsWith(config.prefix + 'time')) {
-            message.channel.send(`time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+            message.channel.send({
+              embed: {
+                color: 3447003,
+                description: `Time: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`
+              }
+            });
           }
         });
         dispatcher.on('end', () => {
@@ -68,31 +125,99 @@ async function bot(client, message, command, args) {
     'join': (message) => {
       return new Promise((resolve, reject) => {
         const voiceChannel = message.member.voiceChannel;
-        if (!voiceChannel || voiceChannel.type !== 'voice') return message.reply('I couldn\'t connect to your voice channel...');
+        if (!voiceChannel || voiceChannel.type !== 'voice') {
+          return message.channel.send({
+            embed: {
+              color: 10181046,
+              description: "I couldn't connect to your voice channel!"
+            }
+          });
+        }
         voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
       });
     },
     'add': (message) => {
       let url = message.content.split(' ')[1];
-      if (url == '' || url === undefined) return message.channel.send(`You must add a YouTube video url, or id after ${config.prefix}add`);
+      if (url == '' || url === undefined) {
+        return message.channel.send({
+          embed: {
+            color: 10181046,
+            description: `You must add a YouTube video url, or id after ${config.prefix}add`
+          }
+        });
+      }
       yt.getInfo(url, (err, info) => {
-        if (err) return message.channel.send('Invalid YouTube Link: ' + err);
+        if (err) {
+          return message.channel.send({
+            embed: {
+              color: 10181046,
+              description: 'Invalid youtube link ' + err
+            }
+          });
+        }
         if (!queue.hasOwnProperty(message.guild.id)) queue[message.guild.id] = {}, queue[message.guild.id].playing = false, queue[message.guild.id].songs = [];
         queue[message.guild.id].songs.push({
           url: url,
           title: info.title,
           requester: message.author.username
         });
-        message.channel.send(`added **${info.title}** to the queue`);
+        message.channel.send({
+          embed: {
+            color: 3447003,
+            author: {
+              name: client.user.username,
+              icon_url: client.user.avatarURL
+            },
+            title: `added **${info.title}** to the queue`,
+            url: info.url,
+            description: info.length,
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL,
+              text: "© " + message.guild
+            }
+          }
+        });
       });
     },
     'queue': (message) => {
-      if (queue[message.guild.id] === undefined) return message.channel.send(`Add some songs to the queue first with ${config.prefix}add`);
+      if (queue[message.guild.id] === undefined) {
+        return message.channel.send({
+          embed: {
+            color: 3447003,
+            description: `Add some songs to the queue first with ${config.prefix}add`
+          }
+        });
+      }
       let tosend = [];
       queue[message.guild.id].songs.forEach((song, i) => {
-        tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);
+        message.channel.send({
+          embed: {
+            color: 3447003,
+            description: `${i+1}. ${song.title} - Requested by: ${song.requester}`
+          }
+        });
       });
-      message.channel.send(`__**${message.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n\`\`\`${tosend.slice(0,15).join('\n')}\`\`\``);
+      message.channel.send({
+        embed: {
+          color: 3447003,
+          author: {
+            name: client.user.username,
+            icon_url: client.user.avatarURL
+          },
+          title: `Music queue for **${message.guild.name}**`,
+          url: "https://github.com/ajmwagar/discordbot",
+          fields: [{
+            name: "Songs Queued [Only next 15 shown]",
+            value: `${tosend.length}`
+          }],
+          timestamp: new Date(),
+          footer: {
+            icon_url: client.user.avatarURL,
+            text: "© Example"
+          }
+        }
+      });
     },
     'reboot': (message) => {
       if (message.author.id == config.adminID) process.exit(); //Requires a node module like Forever to work.
