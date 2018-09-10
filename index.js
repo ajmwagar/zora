b8ee08bf92cc817722d849479ac23a8ce3774200
Nathan <nlaha@outlook.com>
2018-09-09 20:22:16 -0700

    fancy formatting to console
    might not work in pm2

--- index.js
+++ index.js
@@ -2,7 +2,22 @@
     The following code goes into it's own file, and you run this file
     instead of your main bot file.
 */
-
+const logo = require('asciiart-logo');
+const chalk = require("chalk");
+console.log(chalk.keyword('orange')(
+    logo({
+        name: 'ZORA',
+        font: 'doh',
+        lineChars: 15,
+        padding: 5,
+        margin: 2
+    })
+    .emptyLine()
+    .right('version 0.1.0 beta')
+    .emptyLine()
+    .wrap('ZoraBOT was created by Avery Wagar and Nathan Laha')
+    .render()
+));
 const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./src/index.js');
 Manager.spawn(1); // This example will spawn 2 shards (5,000 guilds);
