d7e503caf70b97270ecb1f0baeaebec4cfde10a7
Nathan <nlaha@outlook.com>
2018-09-22 14:27:17 -0700

    fix https

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const fs = require('fs')
+const fs = require('fs');
+const https = require('https');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
