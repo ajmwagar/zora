1405440ae2ef62eb2741c0ce09d91888c03322bb
Nathan <nlaha@outlook.com>
2018-09-22 14:26:10 -0700

    fix

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const fs = require('fs')
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
