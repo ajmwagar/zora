ee6e39e8b06cac007635405fd169ea6a569d84a9
Nathan <nlaha@outlook.com>
2018-09-22 15:00:12 -0700

    fix

--- index.js
+++ index.js
@@ -22,4 +22,40 @@ const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./src/index.js');
 Manager.spawn(1); // This example will spawn 2 shards (5,000 guilds);
 
-require('./server.js');
+/**
+ * Web Interface!
+ * Express Server
+ */
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(50451, () => {
+    console.info('Running on port 50451');
+});
+
+// Routes
+app.use('/api/discord', require('./api/discord'));
+
+app.use((err, req, res, next) => {
+    switch (err.message) {
+        case 'NoCodeProvided':
+            return res.status(400).send({
+                status: 'ERROR',
+                error: err.message,
+            });
+        default:
+            return res.status(500).send({
+                status: 'ERROR',
+                error: err.message,
+            });
+    }
+});
