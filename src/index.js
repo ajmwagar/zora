9c9f7687afdaded2bb774e8ff16e8f1eae5f9fe1
Nathan <nlaha@outlook.com>
2018-09-30 15:42:04 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -211,9 +211,47 @@ client.on("ready", async function () {
   /**
    * Update on start
    */
-  /*
-    // Get from database and sort!
+
+  // Get from database and sort!
+  console.log(chalk.magenta('UPDATED DATA'))
+  const getSort = () => {
+    return UserM.find({}).sort({
+      zcoins: -1
+    }).exec()
+  }
+
+  var sorted = await getSort();
+
+  // Default to 100
+  var top = 25;
+
+  // Add fields
+  var counter = 1;
+  for (var usr in sorted) {
+    var profile = sorted[counter - 1];
+    if (profile) {
+      if (counter <= top && counter <= 25) {
+        if (counter === 1) {
+          UserM.findById(profile._id, function (err, user) {
+            server.stats.richest.id = profile.id;
+            server.stats.richest.name = profile.username;
+            server.stats.richest.zcoins = profile.zcoins;
+            server.stats.richest.level = profile.level;
+            server.save();
+          });
+        }
+      }
+    }
+  }
+
+  /**
+   * Start the main loop, this runs every hour
+   * used to update things such as stats
+   */
+
+  var j = schedule.scheduleJob('0 * * * *', async function () {
     console.log(chalk.magenta('UPDATED DATA'))
+    // Get from database and sort!
     const getSort = () => {
       return UserM.find({}).sort({
         zcoins: -1
@@ -243,46 +281,8 @@ client.on("ready", async function () {
         }
       }
     }
-  */
-  /**
-   * Start the main loop, this runs every hour
-   * used to update things such as stats
-   */
-  /*
-    var j = schedule.scheduleJob('0 * * * *', async function () {
-      console.log(chalk.magenta('UPDATED DATA'))
-      // Get from database and sort!
-      const getSort = () => {
-        return UserM.find({}).sort({
-          zcoins: -1
-        }).exec()
-      }
+  });
 
-      var sorted = await getSort();
-
-      // Default to 100
-      var top = 25;
-
-      // Add fields
-      var counter = 1;
-      for (var usr in sorted) {
-        var profile = sorted[counter - 1];
-        if (profile) {
-          if (counter <= top && counter <= 25) {
-            if (counter === 1) {
-              UserM.findById(profile._id, function (err, user) {
-                server.stats.richest.id = profile.id;
-                server.stats.richest.name = profile.username;
-                server.stats.richest.zcoins = profile.zcoins;
-                server.stats.richest.level = profile.level;
-                server.save();
-              });
-            }
-          }
-        }
-      }
-    });
-  */
 
   // Set activity
   setInterval(function () {
