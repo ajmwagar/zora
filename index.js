88c735b8c1c31c9abcfb30adbadfbb5e9f2d3ad3
Nathan <nlaha@outlook.com>
2018-09-29 20:04:56 -0700

    test

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ app.get('/api/discord/callback', function (req, res) {
             // Sign API requests on behalf of the current user.
             user.sign({
                 method: 'get',
-                url: 'https://dta.dekutree.org'
+                url: 'https://zora.netlify.com'
             })
 
             return res.redirect(`https://zora.netlify.com/#/dashboard?token=${user.accessToken}`)
