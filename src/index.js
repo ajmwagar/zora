904ebe1306981e2b7792645c34fc3e46e7331041
Nathan <nlaha@outlook.com>
2018-09-30 15:24:36 -0700

    richest person

--- src/index.js
+++ src/index.js
@@ -144,6 +144,10 @@ var defaultConfig = new Schema({
       zcoins: {
         type: Number,
         default: 0
+      },
+      level: {
+        type: Number,
+        default: 0
       }
     }
   },
@@ -201,6 +205,81 @@ const ServerM = mongoose.model("Servers", defaultConfig);
 // var memeInterval = setInterval(getMemes, config.reddit.interval * 1000 * 60 * 60);
 
 client.on("ready", () => {
+
+  /**
+   * Update on start
+   */
+
+  // Get from database and sort!
+  const getSort = () => {
+    return UserM.find({}).sort({
+      zcoins: -1
+    }).exec()
+  }
+
+  var sorted = await getSort();
+
+  // Default to 100
+  var top = parseInt(args[0]) || 25;
+
+  // Add fields
+  var counter = 1;
+  for (var usr in sorted) {
+    var profile = sorted[counter - 1];
+    if (profile) {
+      if (counter <= top && counter <= 25) {
+        if (counter === 1) {
+          UserM.findById(profile._id, function (err, user) {
+            server.stats.richest.id = profile.id;
+            server.stats.richest.name = profile.username;
+            server.stats.richest.zcoins = profile.zcoins;
+            server.stats.richest.level = profile.level;
+            server.save();
+          });
+        }
+      }
+    }
+  }
+
+  /**
+   * Start the main loop, this runs every hour
+   * used to update things such as stats
+   */
+
+  var j = schedule.scheduleJob('0 * * * *', function () {
+    // Get from database and sort!
+    const getSort = () => {
+      return UserM.find({}).sort({
+        zcoins: -1
+      }).exec()
+    }
+
+    var sorted = await getSort();
+
+    // Default to 100
+    var top = parseInt(args[0]) || 25;
+
+    // Add fields
+    var counter = 1;
+    for (var usr in sorted) {
+      var profile = sorted[counter - 1];
+      if (profile) {
+        if (counter <= top && counter <= 25) {
+          if (counter === 1) {
+            UserM.findById(profile._id, function (err, user) {
+              server.stats.richest.id = profile.id;
+              server.stats.richest.name = profile.username;
+              server.stats.richest.zcoins = profile.zcoins;
+              server.stats.richest.level = profile.level;
+              server.save();
+            });
+          }
+        }
+      }
+    }
+  });
+
+
   // Set activity
   setInterval(function () {
     client.user.setActivity(`on ${client.guilds.size} servers | ${client.users.size} users`);
